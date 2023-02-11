74ff2b4fb13c0db417eb19143d30012c0a3086b3
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-12 02:54:13 +0300

    files added

--- factory_kurucular.dart
+++ factory_kurucular.dart
@@ -1,1 +1,22 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Ogrenci deha = Ogrenci(165, "Deha");
+  Ogrenci ayse = Ogrenci.factoryKurucusu(-9, "ayse");
+  print(ayse.id);
+  print(ayse.isim);
+}
+
+class Ogrenci {
+  int id = 0;
+  String isim = "";
+
+  Ogrenci(this.id, this.isim);
+
+  factory Ogrenci.factoryKurucusu(int id, String isim) {
+    if (id < 0) {
+      return Ogrenci(5,
+          isim); //factory const. return kullanabiliriz onun dışında return kullanamayız.
+    } else {
+      return Ogrenci(id, isim);
+    }
+  }
+}
