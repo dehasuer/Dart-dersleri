661105b0328465e3ab68246954c0c939a8ba40d3
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-14 01:06:18 +0300

    files added

---
+++ final_const.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  var str = "deha";
+  str = "suer";
+
+  final String str2 = "deha";
+  //str2 = "suer"; //final keywordüyle immutable yapılabilir.
+
+  const String str3 = "deha";
+  //str3 = "suer"; //const keywordüyle de immutable yapılabilir.
+
+  final tarih = DateTime.now(); //runtime
+  //const tarih2 = DateTime.now(); //compile
+
+  final liste = [1, 2, 3];
+  final liste2 = [1, 2, 3];
+  liste.add(
+      2); //final ile listelere eleman ekleyebiliriz fakat yeni bir yapı oluşturamayız.
+
+  if (liste == liste2) {
+    //bellek adreslerini karşılaştırır o yüzden eşit değil.
+    print("eşit");
+  } else {
+    print("eşit değil");
+  }
+}
