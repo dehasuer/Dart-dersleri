24b588c1930fec2a178eff5120413e9419a17a23
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-13 20:56:18 +0300

    file added

---
+++ statik_method_degisken.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  Matematik m1 = Matematik(50, 20);
+  m1.topla();
+  m1.cikar();
+
+  Matematik m2 = Matematik(30, 10);
+  m2.topla();
+  m2.cikar();
+
+  print(Matematik.PI);
+  Matematik.sinifAdiniSoyle();
+
+  print("Toplam işlem sayısı ${Matematik.toplamIslemSayisi}");
+}
+
+class Matematik {
+  //instance variable
+  int birinciSayi = 0;
+  int ikinciSayi = 0;
+  //class variable
+  static double PI = 3.14;
+  static int toplamIslemSayisi = 0;
+  static void sinifAdiniSoyle() {
+    print("ben matematik sınıfıyım");
+  }
+
+  Matematik(this.birinciSayi, this.ikinciSayi);
+
+  void topla() {
+    toplamIslemSayisi++;
+    print("Toplam ${birinciSayi + ikinciSayi}");
+  }
+
+  void cikar() {
+    toplamIslemSayisi++;
+    print("Sonuc ${birinciSayi - ikinciSayi}");
+  }
+}
