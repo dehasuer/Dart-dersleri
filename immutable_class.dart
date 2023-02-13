e1d52bb96922cd6e05ec6b511222c80b50ded8a6
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-14 01:15:23 +0300

    file added

--- immutable_class.dart
+++ immutable_class.dart
@@ -1,1 +1,11 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Student deha = Student(5, "deha");
+  //deha.id = 10; immutable
+}
+
+class Student {
+  final int id;
+  final String isim;
+
+  Student(this.id, this.isim);
+}
